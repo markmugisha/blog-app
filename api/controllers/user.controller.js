aea7fe240293d043ed732503fc5b60abdd76dd9b
Mark Mugisha <141035540+markmugisha@users.noreply.github.com>
2024-02-12 22:13:26 +0300

    create api testing route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,6 @@
+
+
+export const test = (req, res) => {
+  res.json({ message: "API is working okay." });
+}   // This is the controller function that will be called when the route is hit
+
--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 dotenv.config();
 
 mongoose
@@ -18,5 +19,7 @@ const app = express();
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
+
+app.use('/api/user', userRoutes)
 
 
