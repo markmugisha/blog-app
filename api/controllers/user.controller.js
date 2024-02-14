c6b38049b3c9b194ef981c3bd8798756653b0311
Mark Mugisha <141035540+markmugisha@users.noreply.github.com>
2024-02-14 08:51:40 +0300

    create api signup route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -2,5 +2,5 @@
 
 export const test = (req, res) => {
   res.json({ message: "API is working okay." });
-}   // This is the controller function that will be called when the route is hit
+}   // This is the controller function that will be called in the routes when the route api/user/test is hit
 
--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 dotenv.config();
 
 mongoose
@@ -15,11 +16,13 @@ mongoose
  });
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
 
 app.use('/api/user', userRoutes)
+app.use('/api/auth', authRoutes)
 
 
